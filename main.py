ef62fda28e61caf5eb668b1f8b6354431846af5c
Daniel Raymond <dan@raymond.ch>
2021-07-20 23:35:48 -0700

    Slow, kind of working predictor

--- main.py
+++ main.py
@@ -2,64 +2,60 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-
 class Predictor():
 	def __init__(self, series: pd.Series) -> None:
 		self.mapp = self._formatData(series)
 
-	def _formatData(self, series: pd.Series) -> pd.Series:
+	def _formatData(self, series: pd.Series) -> list:
 		# Group data into buckets
-		# ?Perform distance calculations or group buckets logarithmically?
+		# ? Perform distance calculations or group buckets logarithmically ?
 		# Make sure the data is clean
 		s = series.dropna().reset_index(drop=True)
-		return np.round(s)
+		return np.round(s.to_numpy().tolist())
 
-	def getPoint(self, i, length):
+	def getVec(self, i, length):
 		assert i < len(self.mapp) - length, \
 			f"Index out of range: {i} must be less than {len(self.mapp) - length}"
 		return self.mapp[i:i + length]
 
-	def getPointCoord(self, point, coord):
+	def getVecCoord(self, point, coord):
 		assert point < len(self.mapp) - coord, \
 			f"Point out of range: {point} must be less than {len(self.mapp) - coord}"
 		return self.mapp[point + coord]
 
 	def predict(self, data: list) -> float:
-		data = self._formatData(data)
+		data = np.round(data)
 		# Starting indices of all the possible vecs that could match the data
-		guesses = [*range(len(self.mapp) - len(data) - 1)]
-		# Compare each coordinate of data with a vector in the data
-		for coord_idx, coord in enumerate(data):
-			for vec_idx, guess in enumerate(guesses):
-				# If this vector no longer matches the data, remove it
-				if coord != self.getPointCoord(guess, coord_idx):
-					guesses.pop(vec_idx) # ? This might be weird because we are enumerating through the data we are editing
-		# get the last point as the guess for what is happening next in the data
-		answers = [self.getPointCoord(v, len(data)) for v in guesses]
-		if len(answers):
-			return np.median(answers)
+		guesses = []
+		for i in range(len(self.mapp) - len(data)):
+			if np.array_equal(data, self.getVec(i, len(data))): # np.allclose
+				# Get the last point as the guess for what is happening next
+				guess = self.getVecCoord(i, len(data))
+				guesses.append(guess)
+		if len(guesses):
+			return np.median(guesses)
 
 
-def test_model(vec_len=50):
+def test_model(test_len=100, vec_len=50):
 	# Gather data
 	df = pd.read_csv('weatherAUS.csv')['Rainfall']
-	split = round(len(df) * 0.95)
-	train, test = df.head(split), df.tail(len(df) - split)
+	# Vec length is subtracted from the test length
+	train = df.head(len(df) - test_len - vec_len)
+	test = df.tail(test_len + vec_len).dropna().tolist()
 	pred = Predictor(train)
 
 	correct = 0
-	wrong_idxs = []
-	for vec_idx in tqdm(range(len(test) - vec_len)):
-		current = df.loc[vec_idx:vec_idx + vec_len - 1]
-		guess = pred.predict(current)
-		answer = df.loc[vec_idx + vec_len]
-		if guess == answer:
-			correct += 1
-		else:
-			wrong_idxs.append(vec_idx)
-	print(wrong_idxs)
-	print(correct/(len(test) - vec_len))
+	incorrect = 0
+	for vec_idx in tqdm(range(test_len)):
+		current = test[vec_idx:vec_idx + vec_len]
+		if np.any(current):
+			guess = pred.predict(current)
+			answer = test[vec_idx + vec_len]
+			if guess == answer:
+				correct += 1
+			else:
+				incorrect += 1
+	print(correct/(correct + incorrect))
 
 if __name__ == '__main__':
-	test_model()
-
+	test_model(500)
